5c340d7112bfb0a4d519f2b91df60fd268f85cf7
starknet1159 <108061311+starknet1559@users.noreply.github.com>
2022-08-06 14:48:00 +0000

    finish7a

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,7 +8,12 @@ class BooksController < ApplicationController
   end
 
   def index
-    @books = Book.all
+    to = Time.current.at_end_of_day
+    from = (to - 6.day).at_beginning_of_day
+    @books = Book.includes(:favorites).
+      sort { |a,b|
+        b.favorites.includes(:favorites).where(created_at: from...to).size <=>
+        a.favorites.includes(:favorites).where(created_at: from...to).size }
     @book = Book.new
   end
 
